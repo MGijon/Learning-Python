2f2d3e0903ba187b4750f3479398830d49f28521
Unknown <mgijon94@gmail.com>
2017-11-22 15:36:57 +0100

    Fill Between

---
+++ Graphics/Pyplot/Fill_Between.py
@@ -0,0 +1,19 @@
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0.0, 2, 0.01)
+y1 = np.sin(2*np.pi*x)
+y2 = 1.2*np.sin(4*np.pi*x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y1, x, y2, color = 'black')
+ax.fill_between(x, y1, y2, where = y2 > y1, facecolor = 'green')
+ax.fill_between(x, y1, y2, where = y2 <= y1, facecolor = 'red')
+ax.set_title('fill between example 1')
+
+plt.savefig('Fill_Between_example_1.png')
+plt.show()
+
+
+# source : https://matplotlib.org/gallery/pyplots/whats_new_98_4_fill_between.html#sphx-glr-gallery-pyplots-whats-new-98-4-fill-between-py
