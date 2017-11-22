67948f6bc5306b496180b3db3cec0f93b408c401
Unknown <mgijon94@gmail.com>
2017-11-22 15:29:04 +0100

    Dark Background style

---
+++ Graphics/Pyplot/Dark_background_style.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+plt.style.use('dark_background')
+
+fig, ax = plt.subplots()
+
+L = 6
+x = np.linspace(0, L)
+ncolors = len(plt.rcParams['axes.prop_cycle'])
+shift = np.linspace(0, L, ncolors, endpoint=False)
+for s in shift:
+    ax.plot(x, np.sin(x + s), 'o-')
+ax.set_xlabel('x-axis')
+ax.set_ylabel('y-axis')
+ax.set_title("'dark_background' style sheet")
+
+plt.savefig('Dark_Background_style_example_1.png')
+plt.show()
+
+
+# source : https://matplotlib.org/gallery/style_sheets/dark_background.html#sphx-glr-gallery-style-sheets-dark-background-py
