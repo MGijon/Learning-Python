f405ff3c689e44bfbcd1d96ed7d3c1202f54242c
Unknown <mgijon94@gmail.com>
2017-12-10 14:05:05 +0100

    Petersen Graph

---
+++ NetworkX/Petersen_Graph.py
@@ -0,0 +1,13 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.petersen_graph()
+
+plt.subplot(121)
+nx.draw(G, with_labels = True, font_weight = 'bold')
+
+plt.subplot(122)
+nx.draw_shell(G, nlist = [range(5, 10), range(5)], with_labels = True, font_weight = 'bold')
+
+plt.savefig('Petersen_Graph_1.png')
+plt.show()
