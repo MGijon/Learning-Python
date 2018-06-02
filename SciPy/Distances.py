6fe0ad6b5a499bfbfa32fba58562e360926b99f8
Unknown <mgijon94@gmail.com>
2018-06-02 22:49:35 +0200

    updated

---
+++ SciPy/Distances.py
@@ -0,0 +1,12 @@
+import numpy as np
+import scipy
+
+# ahora jugaremos con algunas de las distancias, primero construiremos dos vectores
+X = np.array([x for x in range(0, 11)], dtype = float)
+Y = np.array([x for x in range(10, 21)], dtype = float)
+
+#print(X)
+#print(Y)
+#print(X*Y) # observemos como detalle (no es el objeto de esto) que multiplica elemento a elemento los vectores
+
+# ahora empezamos a jugar con las distancias definidas en scipy
