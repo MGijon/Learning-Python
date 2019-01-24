513ce26fb2efaddf468fa5c5a6c655a4dfc4e5f7
Unknown <mgijon94@gmail.com>
2019-01-24 19:45:47 +0100

    updated

--- PyGame/3line.py
+++ PyGame/3line.py
@@ -18,7 +18,6 @@ carryOn = True
 clock = pygame.time.Clock()
 
 while carryOn:
-    # --- Main event loop
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
               carryOn = False # Flag that we are done so we exit this loop
@@ -26,19 +25,19 @@ while carryOn:
      # --- Game logic should go here
 
      # --- Drawing code should go here
-     # First, clear the screen to white.
-     screen.fill(WHITE)
+     # First, clear the screen to white
+    screen.fill(WHITE)
      #The you can draw different shapes and lines or add text to your background stage.
-     pygame.draw.rect(screen, RED, [55, 200, 100, 70],0)
-     pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
-     pygame.draw.ellipse(screen, BLACK, [20,20,250,100], 2)
+    pygame.draw.rect(screen, RED, [55, 200, 100, 70],0)
+    pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
+    pygame.draw.ellipse(screen, BLACK, [20,20,250,100], 2)
 
 
      # --- Go ahead and update the screen with what we've drawn.
-     pygame.display.flip()
+    pygame.display.flip()
 
      # --- Limit to 60 frames per second
-     clock.tick(60)
+    clock.tick(60)
 
 #Once we have exited the main program loop we can stop the game engine:
 pygame.quit()
